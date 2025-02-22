1dc412b4a22d209c13b1c5930ee5c637bee743a4
Viktor Číhal <viktor.cihal@gmail.com>
2025-02-22 10:57:50 +0200

    Convert softcam installer to console application

--- examples/softcam_installer/softcam_installer.cpp
+++ examples/softcam_installer/softcam_installer.cpp
@@ -3,13 +3,7 @@
 #include <cstring>
 #include <cstdio>
 #include <iostream>
-#include <comdef.h>
-#include <olectl.h>
-#include <initguid.h>
 
-// {AEF3B972-5FA5-4647-9571-358EB472BC9E}
-DEFINE_GUID(CLSID_DShowSoftcam,
-0xaef3b972, 0x5fa5, 0x4647, 0x95, 0x71, 0x35, 0x8e, 0xb4, 0x72, 0xbc, 0x9e);
 
 void Message(const std::string& message)
 {
@@ -22,39 +16,6 @@ std::string ToHex(long x)
     char buff[128];
     std::snprintf(buff, sizeof(buff), "%08lx\n", x);
     return buff;
-}
-
-
-bool IsDllRegistered(const std::string& path)
-{
-    // Load the DLL first to get its class factory
-    HMODULE hmod = LoadLibraryA(path.c_str());
-    if (!hmod)
-    {
-        return false;
-    }
-
-    // Get DllGetClassObject function
-    using DllGetClassObjectFunc = HRESULT(STDAPICALLTYPE*)(REFCLSID, REFIID, LPVOID*);
-    auto DllGetClassObject = (DllGetClassObjectFunc)GetProcAddress(hmod, "DllGetClassObject");
-    
-    if (!DllGetClassObject)
-    {
-        FreeLibrary(hmod);
-        return false;
-    }
-
-    // Try to get the class factory
-    IClassFactory* factory = nullptr;
-    HRESULT hr = DllGetClassObject(CLSID_DShowSoftcam, IID_IClassFactory, (void**)&factory);
-    
-    if (factory)
-    {
-        factory->Release();
-    }
-
-    FreeLibrary(hmod);
-    return SUCCEEDED(hr);
 }
 
 
@@ -98,14 +59,6 @@ int main(int argc, char* argv[])
 
     if (cmd == "register")
     {
-        // Check if already registered
-        if (IsDllRegistered(path))
-        {
-            Message("softcam.dll is already registered in the system");
-            return 0;
-        }
-
-        // Not registered, need admin rights to register
         auto hmod = LoadDLL(path);
         auto RegisterServer = GetProc<HRESULT STDAPICALLTYPE()>(hmod, "DllRegisterServer");
 
@@ -122,7 +75,6 @@ int main(int argc, char* argv[])
     }
     else if (cmd == "unregister")
     {
-        // For unregister, we always need admin rights since we don't know if it's registered
         auto hmod = LoadDLL(path);
         auto UnregisterServer = GetProc<HRESULT STDAPICALLTYPE()>(hmod, "DllUnregisterServer");
 
@@ -130,7 +82,7 @@ int main(int argc, char* argv[])
 
         if (FAILED(hr))
         {
-            Message("Error: unregistration failed (" + ToHex(hr) + ")");
+            Message("Error: registration failed (" + ToHex(hr) + ")");
             return 1;
         }
 
